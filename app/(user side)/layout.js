facba1ca1299b9bae53a9b7e3e49646b214aff5f
Akib-dev1 <akibanj2017@gmail.com>
2025-11-20 06:22:52 +0600

    first commit

--- app/(user side)/layout.js
+++ app/(user side)/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
+import Navbar from "../components/ui/Navbar";
+import Footer from "../components/ui/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,11 +20,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="light">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
